8cc70cb9daf455e07e3dc1829cd41414f7c145ab
speedcom <mateusz.maciaszekhpc@gmail.com>
2014-08-11 18:53:04 +0200

    borth crt & crtKey are optional now, x-auth-token is required only for https flag #1

--- salt-event-hub.py
+++ salt-event-hub.py
@@ -14,21 +14,6 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-try:
-    config_data = open('config.json')
-except IOError:
-    sys.exit("Error: can\'t find config.json")
-else:
-    try:
-        data   = json.load(config_data)
-        token  = data['x_auth_token']
-        crt    = data['crt']
-        crtKey = data['crtKey']
-    except Exception:
-        sys.exit("Error: can\'t read data from config.json")
-    finally:
-        config_data.close()
-
 parser = argparse.ArgumentParser(description='Selection between http and https')
 parser.add_argument('--https', action='store_true', default=False,
                     dest='use_https',
@@ -43,7 +28,7 @@ def event_listener(action):
     from salt.utils.event import SaltEvent
 
     authToken = request.headers['X-AUTH-TOKEN']
-    if authToken != token:
+    if args.use_https and authToken != readFromConfig('x_auth_token'):
         abort(401)
 
     content = request.get_json()
@@ -59,8 +44,25 @@ def event_listener(action):
 def custom_401(error):
     return Response('Wrong X-AUTH-TOKEN', 401, {'Authenticate':'Basic realm="Proper Token Required"'})
 
+def readFromConfig(key):
+  try:
+      config_data = open('config.json')
+  except IOError:
+      sys.exit("Error: can\'t find config.json")
+  else:
+      try:
+          data   = json.load(config_data)
+          value  = data[key]
+      except Exception:
+          sys.exit("Error: can\'t read " + key + " from config.json")
+      finally:
+          config_data.close()
+  return value
+
 if __name__ == '__main__':
     if args.use_https:
-        app.run(host=args.host, port=args.port, debug=True, ssl_context=(crt, crtKey))
+        crtPath = readFromConfig('crtPath')
+        crtKeyPath = readFromConfig('crtKeyPath')
+        app.run(host=args.host, port=args.port, debug=True, ssl_context=(crtPath, crtKeyPath))
     else:
         app.run(host=args.host, port=args.port, debug=True)
